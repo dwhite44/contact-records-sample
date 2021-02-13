ad140423d7b47f7e87a4ce8d0002ea047a9c22cc
Derek White <derekwhite@gmail.com>
2021-02-13 16:07:38 -0600

    Add ability to sort contacts by email / last name, birth-date, and by last name

--- src/contacts/core.clj
+++ src/contacts/core.clj
@@ -1,5 +1,21 @@
 (ns contacts.core)
 
+(defn sort-contacts
+  "Sorts contacts by the given sort key:
+   :email: sorts by email descending, last name ascending
+   :birth-date: sorts by birth date ascending
+   :last-name: sorts by last name descending"
+  [sort-key coll]
+  (case sort-key
+    :email (sort #(let [email-compare (compare (:email %2) (:email %1))]
+                    (if (= 0 email-compare)
+                      (compare (:last-name %1) (:last-name %2))
+                      email-compare))
+                 coll)
+    :birth-date (sort-by :birth-date coll)
+    :last-name (sort-by :last-name #(compare %2 %1) coll)
+    :else (throw (Exception. (str "Invalid sort key " sort-key)))))
+
 (defn import
   [input-file {:keys [sort]
                :or {sort :email}}]
--- test/contacts/core_test.clj
+++ test/contacts/core_test.clj
@@ -1,5 +1,45 @@
 (ns contacts.core-test
   (:require [clojure.test :refer :all]
-            [contacts.core :refer :all]))
+            [contacts.core :as contacts]))
 
-;;TODO: Create Tests
+(deftest contacts-sort-tests
+  (let [test-data [{:last-name "Leonard",
+                    :first-name "Kawhi",
+                    :email "kawhi@clippers.com",
+                    :favorite-color "blue",
+                    :birth-date #inst "1991-06-29T05:00:00.000-00:00"}
+                   {:last-name "Lue",
+                    :first-name "Tyronn",
+                    :email "coach@clippers.com",
+                    :favorite-color "red",
+                    :birth-date #inst "1977-05-03T05:00:00.000-00:00"}
+                   {:last-name "Williams",
+                    :first-name "Lou",
+                    :email "lou.williams@clippers.com",
+                    :favorite-color "black",
+                    :birth-date #inst "1986-10-27T06:00:00.000-00:00"}
+                   {:last-name "Ibaka",
+                    :first-name "Serge",
+                    :email "serge@clippers.com",
+                    :favorite-color "orange",
+                    :birth-date #inst "1989-09-18T05:00:00.000-00:00"}
+                   {:last-name "Drew",
+                    :first-name "Larry",
+                    :email "coach@clippers.com",
+                    :favorite-color "green",
+                    :birth-date #inst "1958-04-02T06:00:00.000-00:00"}]]
+
+    (testing "sort by email (descending), last-name (ascending)"
+      (let [results (contacts/sort-contacts :email test-data)
+            first-names (into [] (map :first-name results))]
+        (is (= first-names ["Serge" "Lou" "Kawhi" "Larry" "Tyronn"]))))
+
+    (testing "sort by birth date (ascending)"
+      (let [results (contacts/sort-contacts :birth-date test-data)
+            first-names (into [] (map :first-name results))]
+        (is (= first-names ["Larry" "Tyronn" "Lou" "Serge" "Kawhi"]))))
+
+    (testing "sort by last name(descending)"
+      (let [results (contacts/sort-contacts :last-name test-data)
+            first-names (into [] (map :first-name results))]
+        (is (= first-names ["Lou" "Tyronn" "Kawhi" "Serge" "Larry"]))))))
